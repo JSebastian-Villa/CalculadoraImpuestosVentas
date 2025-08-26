bdf528dd28f0dfe02b2335397fc610e4c788658b
B12_307_04\b12s307 <jajaja>
2025-08-26 11:17:22 -0500

    Dividir una funcion

--- src/model/calculadora.py
+++ src/model/calculadora.py
@@ -10,13 +10,17 @@ class ErrorCantidadNegativa(Exception):
     """Error caundo la cantidad de productos es negativa"""
 
 
-def calcular(valor_producto, cantidad, impuesto):
+def validar_datos(valor_producto, cantidad, impuesto):
     if valor_producto <= 0:
         raise ErrorPrecioNegativo("Error: valor del producto inválido")
     if cantidad <= 0:
         raise ErrorCantidadNegativa("Error: cantidad inválida")
     if impuesto < 0 or impuesto > 1:
         raise ErrorPorcentajeImpuestoInvalido("Error: impuesto inválido")
+
+# Función para realizar los cálculos
+def calcular(valor_producto, cantidad, impuesto):
+    validar_datos(valor_producto, cantidad, impuesto)
     subtotal = valor_producto * cantidad
     iva = subtotal * impuesto
     total = subtotal + iva
