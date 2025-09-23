b2079556da8ccd9b652cf8f79cf3747a6487a6f9
Julian Osorio <julidoso0123@gmail.com>
2025-09-23 12:51:56 -0500

    Importar excepciones

--- src/model/calculadora.py
+++ src/model/calculadora.py
@@ -1,27 +1,7 @@
-# src/model/calculadora.py
-class ErrorPorcentajeImpuestoInvalido(Exception):
-    def __init__(self, impuesto):
-        super().__init__(
-            f"Impuesto inválido. Debe estar entre 0% y 100%. "
-            f"Actualmente, usted ingresó {impuesto * 100}%."
-        )
+import sys 
+sys.path.append("src")
 
-
-class ErrorPrecioNegativo(Exception):
-    def __init__(self, valor):
-        super().__init__(
-            f"Precio inválido. Debe ser mayor que 0. "
-            f"Actualmente, usted ingresó {valor}."
-        )
-
-
-class ErrorCantidadNegativa(Exception):
-    def __init__(self, cantidad):
-        super().__init__(
-            f"Cantidad inválida. Debe ser un número entero mayor que 0. "
-            f"Actualmente, usted ingresó {cantidad}."
-        )
-
+from model import Excepciones
 
 def validar_datos(valor_producto, cantidad, impuesto):
 
